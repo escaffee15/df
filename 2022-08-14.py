66a10d4daac7e0417a3755a98b9ee9939919a621
escaffee15 <111169635+escaffee15@users.noreply.github.com>
2022-08-15 00:29:59 +0900

    Add files via upload

---
+++ 2022-08-14.py
@@ -0,0 +1,110 @@
+a = 10.5
+print(int(a))
+
+str(100)
+print(str(a))
+
+#프로그램 기능 설명
+#1. 사용자한테 숫자를 입력을 받고
+#2. 사용자가 입력한 값의 10배를 출력
+'''
+x = input('숫자를 입력해 주세요')
+print(input(int(x*10)))
+'''
+'''
+입력한값 = int(input("입력하세요:"))
+print(입력한값*10)
+'''
+int(1.9)
+
+print(int(1.9))
+
+print(int(True))
+
+int(False)
+print(int(False))
+
+n = 'sangmok'
+print(n[0])
+"""
+단 = int(input("원하는 단 입력:"))
+
+print(f'{단} * 1 = {단*1}')
+print(f'{단} * 2 = {단*2}')
+print(f'{단} * 3 = {단*3}')
+print(f'{단} * 4 = {단*4}')
+print(f'{단} * 5 = {단*5}')
+print(f'{단} * 6 = {단*6}')
+print(f'{단} * 7 = {단*7}')
+print(f'{단} * 8 = {단*8}')
+print(f'{단} * 9 = {단*9}')
+
+print("{} X {} = {}". format(단, 1, 단*1))
+
+for i in range(1,10):
+    print(f'{단} X {i} = {단*i}')
+   
+
+for 단 in range(1,10):
+    print(f'{단} X {i} = {단*i}')
+    for i in range(1,10):
+        print(f'{단} X {i} = {단*i}')
+구구단 한번에 다 만드는 소스
+    
+for ? in ?:
+    print("여기서부터")
+    여기까지는
+    포문이고
+여기 포문이 아님
+
+"""
+"""
+answer = 123
+
+while True:
+    b = int(input("입력:"))
+    if b == answer:
+        print("정답")
+        break
+    elif answer < b:
+        print("업")
+    elif answer > b:
+        print("다운")
+
+#1 사용자한테 반복적으로 입력을 받는거
+# for 문 while
+while 1:  <--0 이랑 false 만 아니면 무한적으로 돌아간다
+
+while Ture:
+    입력 : int(input("숫자입력:"))
+
+# 입력 숫자가 정답 보다 클까?
+# 조건문
+    if 정답 > 입력"
+        pint('업')
+    if 정답 < 입력"
+        pint('다운')
+    else:
+        print('정답'
+        break         
+
+"""
+
+"""
+100부터 999까지 숫자에 대해
+X스트라이트 X 아웃으로 맞추는 게임
+1부터 0까지 중복은 없음
+정답 = 123
+"""
+
+canswer = 123
+
+while True:
+    c = int(input("home run"))
+    break
+elif s[0:2] = 1:
+    print("스트라이크")
+elif s[0:2]=2:
+    print("스트라이크")
+elif s[0:2]=3:
+    print("스트라이크")
